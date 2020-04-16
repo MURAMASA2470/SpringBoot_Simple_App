4196ce97cfaa2717c139e3dc11c74355cb937164
MURAMASA2470 <keisuke226214@yahoo.co.jp>
2020-04-16 22:24:22 +0900

    SpringBoot Init

---
+++ src/test/java/com/muramasa/simple/SpringBootSimpleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muramasa.simple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSimpleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
