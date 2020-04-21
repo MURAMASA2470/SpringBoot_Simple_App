efd082be101de4e66e9b7777893946ee99a23541
MURAMASA2470 <keisuke226214@yahoo.co.jp>
2020-04-21 22:11:26 +0900

    DBから取得したuserの一覧表示

--- src/main/java/com/muramasa/simple/controllers/UserListController.java
+++ src/main/java/com/muramasa/simple/controllers/UserListController.java
@@ -1,5 +1,10 @@
 package com.muramasa.simple.controllers;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +12,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class UserListController {
 
+	@Autowired
+	private JdbcTemplate jdbc;
+
 	@GetMapping("/users")
 	public String index(Model model) {
+		List<Map<String, Object>> list = jdbc.queryForList("SELECT * FROM users");
+		
+		model.addAttribute("users", list);
+		
 		return "/users";
 	}
 	
