d5d9b341fe5ac339ebb1e2918c83ec729a088eaa
MURAMASA2470 <keisuke226214@yahoo.co.jp>
2020-04-30 14:31:20 +0900

    flywayの導入

--- src/main/resources/db/migrations/postgresql/V1.0.0__add_user.sql
+++ src/main/resources/db/migrations/postgresql/V1.0.0__add_user.sql
@@ -2,12 +2,14 @@
  test db
 */
 
+drop table if exists public.users cascade;
 CREATE TABLE users (
-  id serial NOT NULL,
+  id bigserial NOT NULL,
   name varchar(50) DEFAULT NULL,
   age integer DEFAULT NULL,
   PRIMARY KEY (id)
 );
+
 
 INSERT INTO users(name, age) VALUES('太郎', 22);
 INSERT INTO users(name, age) VALUES('花子', 21);
