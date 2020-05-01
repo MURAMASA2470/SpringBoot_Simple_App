7942c3f4da1f853caedfd9180624b00098f6bf48
MURAMASA2470 <keisuke226214@yahoo.co.jp>
2020-05-01 11:32:12 +0900

    ORMの拡張

--- src/main/java/com/muramasa/simple/repositories/EmployeeRepository.java
+++ src/main/java/com/muramasa/simple/repositories/EmployeeRepository.java
@@ -1,12 +1,13 @@
 package com.muramasa.simple.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import com.muramasa.simple.models.Employee;
 
 @Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+public interface EmployeeRepository extends JpaRepository<Employee, Long>, JpaSpecificationExecutor<Employee> {
 
 
 	
